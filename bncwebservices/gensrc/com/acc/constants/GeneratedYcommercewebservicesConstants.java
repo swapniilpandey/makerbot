c0445447d7caa8c47a6c809d85df14708042d81a
swapniilpandey <swapnil.a.pandey@accenture.com>
2015-11-20 15:57:59 +0530

    bncwebservice extension for 5.7

---
+++ bncwebservices/gensrc/com/acc/constants/GeneratedYcommercewebservicesConstants.java
@@ -0,0 +1,33 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 20, 2015 3:20:25 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.acc.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedYcommercewebservicesConstants
+{
+	public static final String EXTENSIONNAME = "bncwebservices";
+	public static class TC
+	{
+		public static final String OLDCARTREMOVALCRONJOB = "OldCartRemovalCronJob".intern();
+		public static final String PRODUCTEXPRESSUPDATECLEANERCRONJOB = "ProductExpressUpdateCleanerCronJob".intern();
+	}
+	public static class Attributes
+	{
+		// no constants defined.
+	}
+	
+	protected GeneratedYcommercewebservicesConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
---
+++ bncwebserviceshmc/gensrc/com/acc/constants/GeneratedBncwebserviceshmcConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at Nov 20, 2015 3:20:25 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package com.acc.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedBncwebserviceshmcConstants
+{
+	public static final String EXTENSIONNAME = "bncwebserviceshmc";
+	
+	protected GeneratedBncwebserviceshmcConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
